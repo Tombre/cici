a643690835421e1ebbaf044714227ecf10f3d01e
Tom Leenders <leenders.thomas@gmail.com>
2017-01-25 10:15:15 +0800

    document response helpers

--- app/helpers/response.js
+++ app/helpers/response.js
@@ -1,18 +1,31 @@
 const _ = require('lodash');
 
+/*
+* Choose
+* Chooses a message at random from an array of messages
+*/
 function choose(messages) {
 	return messages[_.random(0, (messages.length -1))];
 }
 
+
+/*
+* Choose For
+* Simple helper function to choose a value from a message map
+*/
 function chooseFor(value, messageMap) {
 	return map[value];
 }
 
+/*
+* Higher Order Fulfill
+* Creates a HOF function which accecpts a fulfillment callback. The fn passed should return an array of [dispatch, response, state].
+*/
 function higherOrderFulfill(fn) {
 	return function(cb) {
 		cb = cb || (dispatch) => dispatch;
 		return function(dispatch, response, state) {
-			return cb(dispatch._bindMethodsTo(fn(dispatch, response, state)), response, state)
+			return cb(...fn(dispatch, response, state))
 		}
 	}
 }
