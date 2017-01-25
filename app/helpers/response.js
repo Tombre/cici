d0b50ecc6d4509e200fa9e320fb5282b3773047f
Tom Leenders <leenders.thomas@gmail.com>
2017-01-25 09:03:52 +0800

    create fn for higher order fullfillments

--- app/helpers/response.js
+++ app/helpers/response.js
@@ -4,4 +4,17 @@ function choose(messages) {
 	return messages[_.random(0, (messages.length -1))];
 }
 
-module.exports = { choose };
+function chooseFor(value, messageMap) {
+	return map[value];
+}
+
+function higherOrderFulfill(fn) {
+	return function(cb) {
+		cb = cb || (dispatch) => dispatch;
+		return function(dispatch, response, state) {
+			return cb(dispatch._bindMethodsTo(fn(dispatch, response, state)), response, state)
+		}
+	}
+}
+
+module.exports = { choose, chooseFor, higherOrderFulfill };
