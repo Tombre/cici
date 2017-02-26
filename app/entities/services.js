ce9b1263a3b75f8d4c9fac3f02ff0de993f32d9c
Tom Leenders <leenders.thomas@gmail.com>
2017-02-26 22:57:49 +0800

    remove depreciated entities

--- app/entities/services.js
+++
@@ -1,7 +0,0 @@
-const createEntity = require('brain/createEntity'); 
-const { profileTypes } = require('memory/user');
-
-let service = createEntity('service')
-	.entries([...Object.keys(profileTypes)])
-
-module.exports = service;
