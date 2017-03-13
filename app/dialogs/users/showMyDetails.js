0e22da352f5ef75fa307be5cd58b8920dd6ce1f9
Tom Leenders <leenders.thomas@gmail.com>
2017-03-13 20:59:58 +0800

    remove who am I with show my details

--- app/dialogs/users/showMyDetails.js
+++ app/dialogs/users/showMyDetails.js
@@ -1,11 +1,28 @@
 const createDialog = require('brain/createDialog');
+const _ = require('lodash');
 const { getUserFromAdapterEvent } = require('memory/user');
+
+/*----------------------------------------------------------
+	Helpers
+----------------------------------------------------------*/
+
+function getUserDefinition(user) {
+	let profiles = _.uniq(user.adapterProfiles.map(profile => profile.adapter)).join(', ');
+	return [
+		`First Name: ${user.givenName} `,
+		`Last Name: ${user.lastName}`,
+		`Role: ${user.role}`,
+		`Email: ${user.email}`,
+		`Profiles: ${profiles}`
+	];
+}
+
 
 /*----------------------------------------------------------
 	Dialog
 ----------------------------------------------------------*/
 
-module.exports = createDialog('who-am-I', dialog => {
+module.exports = createDialog('show-my-details', dialog => {
 
 	/*----------------------------------------------------------
 		Intents
@@ -14,6 +31,10 @@ module.exports = createDialog('who-am-I', dialog => {
 	dialog.registerIntent(
 		dialog.intent('show', true)
 			.userSays(params => [
+				`What are my details?`,
+				`What do you know about me?`,
+				`Show my details`,
+				`Show my user`,
 				`who am I?`,
 				`whoami`,
 				`What is my current user?`,
@@ -27,9 +48,8 @@ module.exports = createDialog('who-am-I', dialog => {
 								.say('You are currently not signed in to any user')
 								.endDialog();
 						}
-						let { lastName, givenName, email} = user;
 						convo
-							.say(`Your user is ${lastName ? (givenName + ' ' + lastName) : givenName}: ${email}`)
+							.say(`Here are your details:\n${getUserDefinition(user).join('\n')}`)
 							.endDialog();
 					})
 			})
